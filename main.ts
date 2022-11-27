17feb09e9b21bdafffb3a78dd6022a68575cafb3
kwongchinfung2a13 <115674616+kwongchinfung2a13@users.noreply.github.com>
2022-11-27 21:15:19 +0800

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -8,14 +8,19 @@ input.onButtonPressed(Button.A, function () {
         set_countdown += -1
     }
 })
-input.onButtonPressed(Button.B, function () {
-    set_number = 20
-    while (set_number > 0) {
-        basic.showNumber(set_number)
-        basic.pause(200)
-        set_number += 1
+input.onButtonPressed(Button.AB, function () {
+    set_countdown = 100
+    while (set_countdown > -100) {
+        basic.showNumber(set_countdown)
+        basic.pause(100)
+        set_countdown += -3
     }
 })
-basic.forever(function () {
-	
+input.onButtonPressed(Button.B, function () {
+    set_number = 10
+    while (set_number > 0) {
+        basic.showNumber(set_number)
+        basic.pause(100)
+        set_number += -2
+    }
 })
